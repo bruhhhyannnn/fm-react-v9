036673e6007c44a160099dc16ac7acb0ef61f638
Bryan Mangapit <bryanjesusmangapit477@gmail.com>
2026-02-17 00:03:54 +0800

    chore(assets): Create and update static assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,25 @@
 import { createRoot } from "react-dom/client";
 import Pizza from "./Pizza";
 
-// we essentially converted this one
-// const App = () => {
-//   return React.createElement(
-//     "div",
-//     {},
-//     React.createElement("h1", {}, "Padre Gino's"),
-
-//     React.createElement(Pizza, {
-//       name: "Pepperoni Pizza",
-//       description: "some description",
-//     }),
-//     React.createElement(Pizza, {
-//       name: "Pizza 2",
-//       description: "some description 2",
-//     }),
-//     React.createElement(Pizza, {
-//       name: "Pizza 3",
-//       description: "some description 3",
-//     }),
-//   );
-// };
-
-// To be come this one
 const App = () => {
   return (
     <div>
       <h1>Padre Ginos - Order Now</h1>
-      <Pizza name="Pepperoni" description="pep, cheese, n stuff" />
-      <Pizza name="Hawaiian" description="ham, pineapple, n stuff" />
-      <Pizza name="Americano" description="french fries, hotdogs, n stuff" />
+      <Pizza
+        name="Pepperoni"
+        description="pep, cheese, n stuff"
+        image="/public/pizzas/pepperoni.webp"
+      />
+      <Pizza
+        name="Hawaiian"
+        description="ham, pineapple, n stuff"
+        image="/public/pizzas/hawaiian.webp"
+      />
+      <Pizza
+        name="Americano"
+        description="french fries, hotdogs, n stuff"
+        image="/public/pizzas/big_meat.webp"
+      />
     </div>
   );
 };
@@ -38,8 +27,4 @@ const App = () => {
 const container = document.querySelector(".root");
 
 const root = createRoot(container);
-// this one is essentially the same
-// root.render(React.createElement(App));
-// as this one
-root.render(<App />); // it will be also return the react element object known as {type, props, ...children}
-// again the render() function will render the react element tree
+root.render(<App />);
--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -1,27 +1,11 @@
-// ! This is essentially what i had in the App.js when learning about react elements, now that is not easy to understand at all compared to the one i created below it.
-// const Pizza = (props) => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h2", {}, props.name),
-//     React.createElement("p", {}, props.description),
-//   ]);
-// };
-
 const Pizza = (props) => {
   return (
     <div className="pizza">
       <h2>{props.name}</h2>
       <p>{props.description}</p>
+      <img src={props.image} alt={props.name} />
     </div>
   );
 };
 
 export default Pizza;
-
-/**
- * this is the same as what we did before
- * const App = () => {
- *  return <h1>hello</h1>
- * }
- *
- * React.createElement("h1", null, "hello")
- */
