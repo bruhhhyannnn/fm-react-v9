5da8e358d2bff08272c5dc26baaf7620490c2373
Bryan Mangapit <bryanjesusmangapit477@gmail.com>
2026-02-16 23:45:57 +0800

    feat(api server): Create our own server with proxy

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  server: {
+    proxy: {
+      // any request starting with /api
+      "/api": {
+        // send to this backend server
+        target: "http://localhost:3000",
+        // change the host header to match target
+        changeOrigin: true,
+      },
+      // any request starting with /public
+      "/public": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [react()],
 });
